fbb32f22899085b6840572c0d32bac2782b2216b
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-03-07 17:58:22 +0000

    salvando

---
+++ ControleFluxo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ControleFluxo
+{   
+    public static void main(String[] args) 
+    {
+        int valor1 ;
+        int valor2 ;
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um numero");
+        valor1 = leitura.nextInt();
+
+        System.out.println("Digite outro numero");
+        valor2 = leitura.nextInt();
+        
+        System.out.println((valor1 > valor2 ? +valor1+ " é maior" : +valor2+ " é maior"));
+
+        if (valor1 > valor2)
+        {
+            System.out.println(valor1+" é maior");
+        }
+
+        else
+        {
+            System.out.println(valor2+" é maior");
+        }
+            
+
+    }
+
+}
